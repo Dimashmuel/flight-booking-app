9153ef9c79dc708603a3b5712a289607bdb3491e
nkmklr <nikitamekler7@gmail.com>
2025-07-08 20:18:05 +0300

    Add basic test for /api/flights/

--- flight_backend/flights/tests.py
+++ flight_backend/flights/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class FlightsEndpointTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_flights_endpoint_returns_200(self):
+        response = self.client.get('/api/flights/')
+        self.assertEqual(response.status_code, 200)
