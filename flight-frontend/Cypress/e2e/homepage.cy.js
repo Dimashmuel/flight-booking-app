21a86ba6a71cedf0d0674213b6115aa996ca9825
nkmklr <nikitamekler7@gmail.com>
2025-07-08 21:40:10 +0300

    Fix CI/CD pipeline for Cypress

--- flight-frontend/Cypress/e2e/homepage.cy.js
+++ flight-frontend/Cypress/e2e/homepage.cy.js
@@ -1,6 +1,6 @@
 describe('Homepage', () => {
   it('loads successfully', () => {
     cy.visit('http://localhost:3000')
-    cy.contains('Flight Booking App') // Update if needed
+    cy.contains('Welcome to the Flight Booking System') // Update if needed
   })
 })
