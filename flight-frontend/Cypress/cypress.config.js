19f5ab941c82bb74f91e1f31d64427bd6bd7bae6
nkmklr <nikitamekler7@gmail.com>
2025-07-08 20:42:11 +0300

    Add Cypress config for E2E tests

--- flight-frontend/Cypress/cypress.config.js
+++ flight-frontend/Cypress/cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
@@ -6,4 +6,4 @@ module.exports = defineConfig({
     specPattern: 'Cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
   },
-})
+});
