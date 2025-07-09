633939225ec72679e1b5511527d40e540b0e20c0
nkmklr <nikitamekler7@gmail.com>
2025-07-09 21:27:58 +0300

    Fix test endpoint URL to match backend route

--- flight_backend/flights/tests.py
+++ flight_backend/flights/tests.py
@@ -1,10 +1,8 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
 
 class FlightsEndpointTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
     def test_flights_endpoint_returns_200(self):
-        response = self.client.get('/api/flights/')
+        response = self.client.get('/flights/')
         self.assertEqual(response.status_code, 200)
