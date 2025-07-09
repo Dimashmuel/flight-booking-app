e68d8ea358a737f86b06cbf675f5b4c19f0e9752
nkmklr <nikitamekler7@gmail.com>
2025-07-09 21:39:03 +0300

    backend test fix

--- flight_backend/flights/tests.py
+++ flight_backend/flights/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
+from flights.models import Flight
+from datetime import datetime
 
-class FlightsEndpointTest(TestCase):
-    def test_flights_endpoint_returns_200(self):
-        response = self.client.get('/flights/')
-        self.assertEqual(response.status_code, 200)
+class FlightModelTest(TestCase):
+    def test_flight_creation(self):
+        flight = Flight.objects.create(
+            origin='CDG',
+            destination='TLV',
+            airline='Air France',
+            date=datetime(2025, 9, 1),
+            time=datetime(2025, 9, 1, 15, 30).time()
+        )
+        self.assertEqual(Flight.objects.count(), 1)
+        self.assertEqual(flight.destination, 'TLV')
